b0b3a6c5d17319f562a5eb6d65e9093fe22775f4
nrmitra <neel.ratan.mitra.1991@gmail.com>
2024-06-04 22:49:29 +0530

    first commit

---
+++ Practice_1/Practice_1/views.py
@@ -0,0 +1,9 @@
+from django.http import HttpResponse
+from django.shortcuts import render
+
+def homepage(request):
+    return render(request,'home.html')
+    #return HttpResponse("Hello World!")
+
+def about(request):
+    return HttpResponse("About Page!")
